9e9e65080fcef30028bb80592cdf2d04c8915e41
thebao13 <bao.pt.64cntt@ntu.edu.vn>
2025-02-17 16:23:42 +0700

    tạo ví dụ nhập xuất
    ghi nhận sự thay dổi

---
+++ ViDu/src/thebao/vidu/NhapXuat.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package thebao.vidu;
+
+/**
+ * 
+ */
+public class NhapXuat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
