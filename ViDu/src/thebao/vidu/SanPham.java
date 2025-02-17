b0f4b8e82ce4eb601452f8c40c116547470b02f8
thebao13 <bao.pt.64cntt@ntu.edu.vn>
2025-02-17 16:52:50 +0700

    Xây dựng các đối tượng, viết các phương thức getter và setter. viết tostring
    vừa viết tay  vừa tao tác theo hương dẫn của thầy

--- ViDu/src/thebao/vidu/SanPham.java
+++ ViDu/src/thebao/vidu/SanPham.java
@@ -1,5 +1,52 @@
 package thebao.vidu;
 
 public class SanPham {
-
+		//khởi tạo được lớp 
+		// định nghĩa các thuộc tính
+	int maSP;
+	String tenSP;
+	String loaiSP;
+	String anhSP;
+	public SanPham() {
+		//khởi tạo giá trị ban đầu nếu có cho các thuộc tính
+		
+	}
+	public SanPham(int _maSP, String _tenSP, String _loaiSP,String _anhSP) {
+		//khởi tạo giá trị ban đầu nếu có cho các thuộc tính
+		maSP = _maSP;
+		tenSP = _tenSP;
+		loaiSP = _loaiSP;
+		anhSP = _anhSP;
+		
+	}
+	public int getMaSP() {
+		return maSP;
+	}
+	public void setMaSP(int maSP) {
+		this.maSP = maSP;
+	}
+	public String getTenSP() {
+		return tenSP;
+	}
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+	public String getAnhSP() {
+		return anhSP;
+	}
+	public void setAnhSP(String anhSP) {
+		this.anhSP = anhSP;
+	}
+	@Override
+	public String toString() {
+		return "SanPham [maSP=" + maSP + ", tenSP=" + tenSP + ", loaiSP=" + loaiSP + ", anhSP=" + anhSP + "]";
+	}
+	
+	
 }
