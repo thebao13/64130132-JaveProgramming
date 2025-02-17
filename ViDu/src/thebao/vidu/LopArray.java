6ceefc57df6981aee8ebb8345466843398ceb18e
thebao13 <bao.pt.64cntt@ntu.edu.vn>
2025-02-17 17:21:06 +0700

    đã khai thác được  biến arraylist sau khi sửa 7749 lỗi

--- ViDu/src/thebao/vidu/LopArray.java
+++ ViDu/src/thebao/vidu/LopArray.java
@@ -1,6 +1,25 @@
 package thebao.vidu;
 import java.util.ArrayList;
-public class LopArray {
-	 ArrayList<SanPham> dsSanPham;
-	 
+public class LopArray 
+{
+		public static void main (String [] args) {
+	     ArrayList<SanPham> dsSanPham;
+	     dsSanPham = new ArrayList<SanPham>();
+	    //   SanPham sp1 = new SanPham(0, "kem", "Giải khát", null);
+	         SanPham sp2 = new SanPham(1, "cơm", "đồ ăn", null);
+	         SanPham sp3 = new SanPham(2, "Phở", "đồ ăn", null);
+	         
+	         dsSanPham.add(new SanPham(0, "kem", "Giải khát", null));
+	         dsSanPham.add(sp2);
+	         dsSanPham.add(sp3);
+	     
+
+	     // Phương thức main để chạy chương trình
+	     for (int i =0 ; i < dsSanPham.size(); i++) {
+	    	 SanPham sp = dsSanPham.get(i);
+	    	 System.out.println(sp.toString());
+	     }
+	     
+	  
+		}
 }
