7f21e735eb7904a5d50a74f6a20d6aa1f2224d10
thebao13 <bao.pt.64cntt@ntu.edu.vn>
2025-02-17 16:33:03 +0700

    Chương trình tính tổng

--- ViDu/src/thebao/vidu/NhapXuat.java
+++ ViDu/src/thebao/vidu/NhapXuat.java
@@ -2,7 +2,7 @@
  * 
  */
 package thebao.vidu;
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -13,7 +13,19 @@ public class NhapXuat {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Chuong trình tính Tổng \n");
+		Scanner banPhim = new Scanner(System.in);
+		
+		System.out.print("Nhập vào số A : ");
+		double a = banPhim.nextDouble();
+		
+		System.out.print("Nhập vào số b : ");
+		double b = banPhim.nextDouble();
+		
+		
+		double tong = a+b;
+		System.out.print("Tổng là : " + tong);
+		banPhim.close();
 	}
 
 }
